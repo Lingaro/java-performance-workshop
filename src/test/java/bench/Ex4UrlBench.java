681261f0c6d3287e075e16a6b0212b79f49b0794
Krzysztof Rzymkowski <krzysztof.rzymkowski@lingaro.com>
2018-12-29 18:56:34 +0100

    make URL benchmark avoid DNS cache

--- src/test/java/bench/Ex4UrlBench.java
+++ src/test/java/bench/Ex4UrlBench.java
@@ -13,25 +13,24 @@ import java.net.URISyntaxException;
 import java.net.URL;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @BenchmarkMode(Mode.Throughput)
-@Measurement(iterations = 3, time = 1)
+@Measurement(iterations = 5, time = 1)
 public class Ex4UrlBench extends BenchmarkTest {
 
     @Benchmark
     public boolean url() throws MalformedURLException {
-        return new URL("https://wp.pl").equals(new URL("https://www.wp.pl"));
+        return new URL("https://wp.pl").equals(new URL("https://" + System.currentTimeMillis() + ".wp.pl"));
     }
 
     @Benchmark
     public boolean uri() throws URISyntaxException {
-        return new URI("https://wp.pl").equals(new URI("https://www.wp.pl"));
+        return new URI("https://wp.pl").equals(new URI("https://" + System.currentTimeMillis() + ".wp.pl"));
     }
 
     @Test
     public void urlTest() throws MalformedURLException {
-        assertTrue(url());
+        assertFalse(url());
     }
 
     @Test
